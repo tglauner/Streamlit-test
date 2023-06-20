aebb6cc62bf34f274cd9c7aa92ad7e047990c824
Tim Glauner <glaunertim@gmail.com>
2023-06-19 22:36:24 -0400

    Working version but with no REST calls as of now

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+snow = st.button('Snow!')
+if snow:
+    st.snow()
+
+tab1, tab2, tab3, tab4 = st.tabs(["RFQ", "Hedge", "Playground", 'Market Data'])
 
 
-tab1, tab2 = st.tabs(["RFQ", "Hedge"])
 
 with tab1:
     st.header("Advances RFQ")
@@ -9,17 +16,32 @@ with tab1:
     accepted_maturities = ['1Y','2Y','3Y']
     accepted_pay_freq = ['M','Q','S','A']
     with st.form(key='advance_rfq'):
-        maturity = st.text_input('Maturity','3Y')
-        if maturity in accepted_maturities:
-            st.write('ok')
-        else:
-            st.error('Invalid maturity:'+str(accepted_maturities))           
-        pay_freq = st.text_input('Pay Frequency','Q')
-        st.form_submit_button('Quote')
-    st.write('Fixed Rate: ')
+        notional = st.number_input('Notional')
+        maturity = st.selectbox('Maturity',accepted_maturities)         
+        pay_freq = st.selectbox('Pay Frequency',accepted_pay_freq)
+        submitted_rfq = st.form_submit_button('Request for Quote')
+    if submitted_rfq:
+        st.write('REST call to be implemented')
+        st.write('Fixed Rate: 3.12%')
 
 with tab2:
     st.header("Hedge with Asset Swap")
     with st.form(key='hedge'):
-        st.form_submit_button("Execute Hedge")
-    st.write('Generated Trade Id:')
+        submitted_hedge = st.form_submit_button("Execute Hedge")
+    
+        if submitted_hedge:
+            st.write('REST call to be implemented')
+            st.write('Generated Trade Id: 65234SU')
+
+with tab3:
+    df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
+    st.dataframe(df)
+
+with tab4:
+    st.header('Market Data')
+    #Fill with REST call
+    d = {'Date': ['1D','1W','2W','3W', '1M'], 
+         'Instrument': ['Overnight', 'Swap', 'Swap', 'Swap', 'Swap'], 
+         'Rate': [1,1,1,1,1]}
+    mktData = pd.DataFrame(data=d)
+    st.write(mktData)
